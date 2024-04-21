cd13214335de7be684445671dfe9f38f6112d71b
Azal-Amer <azalamer6@gmail.com>
2024-04-21 15:39:15 -0500

    Fixed envelope function  and math

--- double-slit-experiment/src/WaveCanvas.js
+++ double-slit-experiment/src/WaveCanvas.js
@@ -5,18 +5,39 @@ import React, { useRef, useEffect } from 'react';
 const WaveCanvas = ({ points, intensity }) => {
   const canvasRef = useRef(null);
 
-  useEffect(() => {
+  useEffect(() => { 
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     const width = canvas.width;
     const height = canvas.height;
 
     ctx.clearRect(0, 0, width, height); // Clear the canvas before drawing
-
     // Draw vertical lines based on the intensity array
+    function mapIntensity(intensity,maxIntensity){
+      // we're just going to make it modify the tolerance on the data
+      const ceilingCover = .05*maxIntensity;
+      let newIntensities = intensity.map((intensity) =>
+        intensity/ceilingCover
+      );
+      // if a value is greater than 1, make it 1
+      // if a value is less than 0, make it 0
+      // newIntensities = newIntensities.map((intensity) =>
+      //   intensity > 1 ? 1 : intensity
+      // );
+
+         
+      return newIntensities;
+    }
     points.forEach((point, index) => {
       const x = (index / points.length) * width;
-      const brightness = intensity[index] * 255;  // Scale brightness to 0-255
+      // get the maximum value of the intensity array
+      const maxIntensity = Math.max(...intensity);
+      // divide all in the intensity array by the max intensity
+      
+      intensity =  mapIntensity(intensity,maxIntensity);
+
+      // scale the intensity array to 0-255
+      const brightness = intensity[index] *255;  // Scale brightness to 0-255
       const color = `rgb(${brightness},${brightness},${brightness})`;
       ctx.strokeStyle = color;
       ctx.beginPath();
--- double-slit-experiment/src/WaveInterference.js
+++ double-slit-experiment/src/WaveInterference.js
@@ -7,12 +7,19 @@ import ParticleCanvas from './ParticleCanvas';
 import ObserverEffectCanvas from './ObserverEffectCanvas';
 
 const WaveInterference = ({ wavelength, distance, slitSeparation, showParticles, observerEffect, numPhotons }) => {
+  // wave interference defines the 
   const points = math.range(-10, 10, 0.02).toArray();
   const intensity = points.map(x => {
     const theta = Math.atan(x / distance);
     const pathDifference = slitSeparation * 1e-6 * Math.sin(theta);
     const phaseDifference = (2 * Math.PI / (wavelength * 1e-9)) * pathDifference;
-    return Math.pow(Math.cos(phaseDifference / 2), 2);
+    const dropoffTerm = 3.14*slitSeparation*x/(wavelength*1e-9)*distance;
+    // we need to define slit width
+    const slitWidth = 1e-3;
+    const beta = Math.PI*slitWidth*Math.sin(theta)/(wavelength*1e-9);
+    const alpha = Math.PI*slitSeparation*Math.sin(theta)/(wavelength*1e-9);
+    return Math.cos(alpha)**2*Math.pow(Math.sin(beta)/beta,2);
+
   });
 
   if (observerEffect) {
