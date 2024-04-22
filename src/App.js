0a857216e829ae59d66cae1c7b702e50fd506874
Azal-Amer <azalamer6@gmail.com>
2024-04-21 22:50:28 -0500

    meep

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import WaveInterference from './WaveInterference';
 import DoubleSlit from "./diagramComponent";
+import { MathJaxContext } from 'better-react-mathjax'; // Assuming 'better-react-mathjax' package is used
 function App() {
    const [htmlContent, setHtmlContent] = useState("");
 
@@ -31,7 +32,6 @@ function App() {
               window.MathJax.typesetPromise();
             }
           }, 3000); // Wait for 3 seconds
-
           // Clear the timeout if the component unmounts
           return () => clearTimeout(timeout);
         }
@@ -44,124 +44,125 @@ function App() {
   
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Double Slit Experiment Simulation</h1>
-      </header>
-      <div style={{ display: "flex", flexGrow: 1 }}>
-        <div style={{ display: "flex" }}>
-          <div
-            className="controls"
-            style={{ marginRight: "1rem", width: "500px" }}
-          >
-            <label>
-              Wavelength (nm):
-              <input
-                type="range"
-                min="380"
-                max="750"
-                value={wavelength}
-                onChange={(e) => setWavelength(Number(e.target.value))}
-              />
-              {wavelength} nm
-            </label>
-            <label>
-              Distance to Screen (mm):
-              <input
-                type="range"
-                min="1000"
-                max="5000"
-                value={distance}
-                onChange={(e) => setDistance(Number(e.target.value))}
-              />
-              {distance} mm
-            </label>
-            <label>
-              Slit Seperation (μm):
-              <input
-                type="range"
-                min="400"
-                max="1500"
-                value={slitSeparation}
-                onChange={(e) => setSlitSeparation(Number(e.target.value))}
-              />
-              {slitSeparation} μm
-            </label>
-            <label>
-              Slit width(μm):
-              <input
-                type="range"
-                min="300"
-                max="2000"
-                value={slitWidth}
-                onChange={(e) => setSlitWidth(Number(e.target.value))}
-              />
-              {slitWidth} μm
-            </label>
-            <label>
-              Show Particles:
-              <input
-                type="checkbox"
-                checked={showParticles}
-                onChange={(e) => setShowParticles(e.target.checked)}
-              />
-            </label>
+    <MathJaxContext>
+      <div className="App">
+        <header className="App-header">
+          <h1>Double Slit Experiment Simulation</h1>
+        </header>
+        <div style={{ display: "flex", flexGrow: 1 }}>
+          <div style={{ display: "flex" }}>
+            <div
+              className="controls"
+              style={{ marginRight: "1rem", width: "500px" }}
+            >
+              <label>
+                Wavelength (nm):
+                <input
+                  type="range"
+                  min="380"
+                  max="750"
+                  value={wavelength}
+                  onChange={(e) => setWavelength(Number(e.target.value))}
+                />
+                {wavelength} nm
+              </label>
+              <label>
+                Distance to Screen (mm):
+                <input
+                  type="range"
+                  min="1000"
+                  max="5000"
+                  value={distance}
+                  onChange={(e) => setDistance(Number(e.target.value))}
+                />
+                {distance} mm
+              </label>
+              <label>
+                Slit Seperation (μm):
+                <input
+                  type="range"
+                  min="400"
+                  max="1500"
+                  value={slitSeparation}
+                  onChange={(e) => setSlitSeparation(Number(e.target.value))}
+                />
+                {slitSeparation} μm
+              </label>
+              <label>
+                Slit width(μm):
+                <input
+                  type="range"
+                  min="300"
+                  max="2000"
+                  value={slitWidth}
+                  onChange={(e) => setSlitWidth(Number(e.target.value))}
+                />
+                {slitWidth} μm
+              </label>
+              <label>
+                Show Particles:
+                <input
+                  type="checkbox"
+                  checked={showParticles}
+                  onChange={(e) => setShowParticles(e.target.checked)}
+                />
+              </label>
               <label>
                 Insensity Graph:
                 <input
                   type="checkbox"
                   checked={observerEffect}
                   onChange={(e) => setObserverEffect(e.target.checked)}
+                />
+              </label>
 
-                />
-              </label>
-            
-            {showParticles && ( // Conditionally render "Number of Photons" if showParticles is true
-              <label htmlFor="numPhotons">
-                Number of Photons:
-                <input
-                  id="numPhotons"
-                  type="range"
-                  min="100"
-                  max="20000"
-                  step="100"
-                  value={numPhotons}
-                  onChange={(e) => setNumPhotons(Number(e.target.value))}
-                />
-                {numPhotons}
-              </label>
-            )}
-          </div>
-          <div style={{ flex: 1 }}>
-            <div className="WaveInterferenceContainer">
-              <WaveInterference
-                wavelength={wavelength}
-                distance={distance}
-                slitSeparation={slitSeparation}
-                showParticles={showParticles}
-                observerEffect={observerEffect}
-                numPhotons={numPhotons}
-                slitWidth={slitWidth} // Pass the number of photons to the WaveInterference component
-              />
-              <div>
-                <DoubleSlit
+              {showParticles && ( // Conditionally render "Number of Photons" if showParticles is true
+                <label htmlFor="numPhotons">
+                  Number of Photons:
+                  <input
+                    id="numPhotons"
+                    type="range"
+                    min="100"
+                    max="20000"
+                    step="100"
+                    value={numPhotons}
+                    onChange={(e) => setNumPhotons(Number(e.target.value))}
+                  />
+                  {numPhotons}
+                </label>
+              )}
+            </div>
+            <div style={{ flex: 1 }}>
+              <div className="WaveInterferenceContainer">
+                <WaveInterference
                   wavelength={wavelength}
                   distance={distance}
                   slitSeparation={slitSeparation}
                   showParticles={showParticles}
-                  slitWidth={slitWidth}
+                  observerEffect={observerEffect}
+                  numPhotons={numPhotons}
+                  slitWidth={slitWidth} // Pass the number of photons to the WaveInterference component
                 />
+                <div>
+                  <DoubleSlit
+                    wavelength={wavelength}
+                    distance={distance}
+                    slitSeparation={slitSeparation}
+                    showParticles={showParticles}
+                    slitWidth={slitWidth}
+                  />
+                </div>
               </div>
-            </div>
-            <div style={{ flex: 1 }} className="html-content-column">
-              <div dangerouslySetInnerHTML={createMarkup(htmlContent)} />
-            </div>
+              <div style={{ flex: 1 }} className="html-content-column">
+                <div dangerouslySetInnerHTML={createMarkup(htmlContent)} />
+              </div>
 
-            {/* HTML content column */}
+              {/* HTML content column */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </MathJaxContext>
   );
 }
 
