81ecef112da7f31bfa89384cbd575758f9a5b221
Azal-Amer <azalamer6@gmail.com>
2024-04-23 14:39:17 -0500

    idunno

--- src/ObserverEffectCanvas.js
+++ src/ObserverEffectCanvas.js
@@ -24,20 +24,38 @@ const ObserverEffectCanvas = ({
     }
   }, [points, intensity, measure]);
 
-  function drawPhotons(ctx, width, height, points, intensity) {
+  // function drawPhotons(ctx, width, height, points, intensity) {
     
+  //   const scale = height / Math.max(...intensity);
+  //   const maxIntensity = Math.max(...intensity);
+  //   ctx.fillStyle = wavelengthToColor(wavelength, 1);
+  //   points.forEach((point, index) => {
+  //     const x = (index / points.length) * width;
+  //     const y = height - intensity[index] * scale; // Invert y-axis for canvas
+  //     const brightness = intensity[index] / maxIntensity; // Normalize brightness
+  //     const dotSize = dotRadius;
+  //     ctx.beginPath();
+  //     ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
+  //     ctx.fill();
+  //   });
+  // }
+  function drawPhotons(ctx, width, height, points, intensity) {
     const scale = height / Math.max(...intensity);
     const maxIntensity = Math.max(...intensity);
-    ctx.fillStyle = wavelengthToColor(wavelength, 1);
+    ctx.strokeStyle = wavelengthToColor(wavelength, 1);
+    ctx.lineWidth = 2; // Adjust line width as needed
+    ctx.beginPath();
     points.forEach((point, index) => {
       const x = (index / points.length) * width;
       const y = height - intensity[index] * scale; // Invert y-axis for canvas
       const brightness = intensity[index] / maxIntensity; // Normalize brightness
-      const dotSize = dotRadius;
-      ctx.beginPath();
-      ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
-      ctx.fill();
+      if (index === 0) {
+        ctx.moveTo(x, y); // Move to the first point
+      } else {
+        ctx.lineTo(x, y); // Draw line to the current point from the previous one
+      }
     });
+    ctx.stroke(); // Stroke the path to draw the lines
   }
 
   return (
